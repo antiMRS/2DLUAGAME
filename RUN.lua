6aa9f81025cced3c6412efb88c20fcb32662a0f0
antiMRS <mine2009_1p@inbox.ru>
2025-08-24 13:46:49 +0300

    Signed-off-by: antiMRS <mine2009_1p@inbox.ru>

---
+++ RUN.lua
@@ -0,0 +1,8 @@
+package.cpath = "./DLLmodules/?.dll"
+package.path = "./modules/?.lua"
+
+al = require("moonal")
+ft = require("moonfreetype")
+gl = require("moongl")
+gw = require("moonglfw")
+im = require("moonimage")
